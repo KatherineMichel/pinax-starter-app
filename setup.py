e3f2290e2f07aa73a12835df9099277c1560b2dc
KatherineMichel <kthrnmichel@gmail.com>
2019-06-11 22:27:00 -0500

    Updating to beta

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "pytest": ["pytest", "pytest-django"]
     },
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
         "Framework :: Django",
         "Framework :: Django :: 2.0",
